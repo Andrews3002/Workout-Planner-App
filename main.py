408813b898e792c63a09cd1e2773638dd6455c4e
Andrews3002 <alexangeloandrews@gmail.com>
2024-11-14 02:46:25 -0400

    migration files

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from functools import wraps
 from sqlalchemy.exc import IntegrityError
 from flask_jwt_extended import (JWTManager, create_access_token, get_jwt_identity, jwt_required, current_user, set_access_cookies,
 unset_jwt_cookies, verify_jwt_in_request)
+from flask_migrate import Migrate
 
 from models import User, Workout, Exercise, db
 
@@ -44,6 +45,7 @@ def create_app():
     return app
 
 app = create_app()
+migrate = Migrate(app, db)
 jwt = JWTManager(app)
 
 @jwt.user_identity_loader
